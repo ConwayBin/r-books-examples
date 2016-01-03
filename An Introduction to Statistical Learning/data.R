e42c3cd36d3e40851f4bf1414250a85503afe009
Robert Jonczy <robert.jonczy@gmail.com>
2016-01-03 17:00:59 +0100

    added An Introduction to Statistical Learning

---
+++ An Introduction to Statistical Learning/data.R
@@ -0,0 +1,12 @@
+advertisingDataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv"
+#autoDataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Auto.data"
+autoDataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Auto.csv"
+collegeDataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/College.csv"
+ch10ex11DataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Ch10Ex11.csv"
+creditDataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Credit.csv"
+income1DataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Income1.csv"
+income2DataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Income2.csv"
+heartDataUrl <- "http://www-bcf.usc.edu/~gareth/ISL/Heart.csv"
+
+library(ISLR)
+library(MASS)
