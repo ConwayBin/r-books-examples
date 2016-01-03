f865bafe15abdfd700282808e53c381648da1470
Robert Jonczy <robert.jonczy@gmail.com>
2016-01-03 18:40:25 +0100

    few improvments after executing examples

--- Building a Recommendation System with R/Chapter 2/Boosting.r
+++ Building a Recommendation System with R/Chapter 2/Boosting.r
@@ -1,12 +1,17 @@
 #Boosting in R
-Install.packages(“gbm”)
+#Install.packages(“gbm”)
+
 library(gbm)
 data(iris)
-sample = iris[sample(nrow(iris)),]
-train = sample[1:105,]
-test = sample[106:150,]
+
+sample <- iris[sample(nrow(iris)),]
+
+train <- sample[1:105,]   # 70% training
+test <- sample[106:150,]  # 30% test
+
 str(train)
-model = gbm(Species~.,data=train,distribution="multinomial",n.trees=5000,interaction.depth=4)
+model <- gbm(Species ~ . , data = train, distribution = "multinomial", n.trees = 5000, interaction.depth = 4)
 summary(model)
-pred = predict(model,newdata=test[,-5],n.trees=5000)
+
+pred <- predict(model, newdata = test[ ,-5], n.trees = 5000)
 pred
--- Building a Recommendation System with R/Chapter 2/SVM.r
+++ Building a Recommendation System with R/Chapter 2/SVM.r
@@ -1,27 +1,19 @@
-install.packages("e1071")
+#install.packages("e1071")
 #SVM
 library(e1071)
+
 data(iris)
-sample = iris[sample(nrow(iris)),]
-train = sample[1:105,]
-test = sample[106:150,]
-tune =tune(svm,Species~.,data=train,kernel ="radial",scale=FALSE,ranges =list(cost=c(0.001,0.01,0.1,1,5,10,100)))
+
+sample <- iris[sample(nrow(iris)), ]
+
+train <- sample[1:105, ]
+test <- sample[106:150, ]
+
+tune <- tune(svm, Species ~ . ,data = train, kernel = "radial", scale = FALSE, ranges = list(cost = c(0.001,0.01,0.1,1,5,10,100)))
 tune$bestmodel
+
+model <- svm(Species ~ . , data = train, kernel ="radial", cost = 10, scale = FALSE)
 model
-'
-Call:
-best.tune(method = svm, train.x = Species ~ ., data = train, ranges = list(cost = c(0.001, 
-    0.01, 0.1, 1, 5, 10, 100)), kernel = "radial", scale = FALSE)
 
-
-Parameters:
-   SVM-Type:  C-classification 
- SVM-Kernel:  radial 
-       cost:  10 
-      gamma:  0.25 
-
-Number of Support Vectors:  25
-'
-model =svm(Species~.,data=train,kernel ="radial",cost=10,scale=FALSE)
-pred = predict(model,test)
+pred <- predict(model, test)
 pred
