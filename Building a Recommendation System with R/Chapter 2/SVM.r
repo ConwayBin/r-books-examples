f532a2d235a1c21a20e265bbbd45a040e77eeeaa
Robert Jończy <robert.jonczy@gmail.com>
2016-01-24 22:54:40 +0100

    small fix

--- Building a Recommendation System with R/Chapter 2/SVM.r
+++ Building a Recommendation System with R/Chapter 2/SVM.r
@@ -9,8 +9,8 @@ sample <- iris[sample(nrow(iris)), ]
 train <- sample[1:105, ]
 test <- sample[106:150, ]
 
-tune <- tune(svm, Species ~ . ,data = train, kernel = "radial", scale = FALSE, ranges = list(cost = c(0.001,0.01,0.1,1,5,10,100)))
-tune$bestmodel
+tune <- tune(svm, Species ~ . ,data = train, kernel = "radial", scale = FALSE, ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
+tune$best.model
 
 model <- svm(Species ~ . , data = train, kernel ="radial", cost = 10, scale = FALSE)
 model
