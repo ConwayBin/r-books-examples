f2c70695f4d44df9ae8a2628846be518b91ff23a
Robert Jończy <robert.jonczy@gmail.com>
2016-01-25 23:08:53 +0100

    added some comments

--- Building a Recommendation System with R/Chapter 2/Decision_Trees.r
+++ Building a Recommendation System with R/Chapter 2/Decision_Trees.r
@@ -9,9 +9,10 @@ sample <- iris[sample(nrow(iris)), ]
 train <- sample[1:105,]
 test <- sample[106:150,]
 
-model <- tree(Species ~ ., train)
+model <- tree(formula = Species ~ ., data = train)
 
 summary(model)
 
+# test[,-5] means without column 'Species'
 pred <- predict(model, test[,-5], type = "class")
 pred
--- Building a Recommendation System with R/Chapter 2/SVM.r
+++ Building a Recommendation System with R/Chapter 2/SVM.r
@@ -9,10 +9,15 @@ sample <- iris[sample(nrow(iris)), ]
 train <- sample[1:105, ]
 test <- sample[106:150, ]
 
+# Now, we will see that the SVM() method is called with the tune() method, which
+# does cross validation and runs the model on different values of the cost parameters
 tune <- tune(svm, Species ~ . ,data = train, kernel = "radial", scale = FALSE, ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
 tune$best.model
 
-model <- svm(Species ~ . , data = train, kernel ="radial", cost = 10, scale = FALSE)
+# check summary of tune
+summary(tune)
+
+model <- svm(Species ~ . , data = train, kernel ="radial", cost = 5, scale = FALSE, gamma = 0.25)
 model
 
 pred <- predict(model, test)
